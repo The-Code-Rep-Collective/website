f1c60a93ed67700e8cd6ebba13005b8535b5c7ef
Whitney Lubin <justwhitneylubin@gmail.com>
2019-08-10 15:06:47 -0400

    post request

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -9,6 +9,20 @@ module Api
                 location = Location.find(params[:id]);
                 render json: {status: 'SUCCESS', message: 'Loaded Location', data:location}, status: :ok
             end
+            def create
+                location = Location.new(location_params)
+            
+                if location.save
+                    render json: {status: 'SUCCESS', message: 'Saved location', data:location}, status: :ok
+                else
+                    render json: {status: 'ERROR', message: 'Location not saved', data:location.errors}, status: :unprocessable_entity
+            end
+
+            private
+
+            def location_params
+                params.permit(:name, :address, :phone, :description, :units)
+            end
         end
     end
 end
