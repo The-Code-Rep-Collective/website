196dac172d5894a1867f6fe5e06653178cf487db
Whitney Lubin <justwhitneylubin@gmail.com>
2019-08-10 09:09:54 -0400

    regenerate controllers for locations and zipcode

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,10 +1,2 @@
-# frozen_string_literal: true
-
 class LocationsController < ApplicationController
-  def index
-    @locations = locations.ApplicationController
-    @locations = @locations.where(housing_type: params[:housing_type]) if params[:housing_type].present?
-
-    render json: @locations
-  end
 end
