4e0ac86dec0a855cac2e46c0fd92bddf99caef76
Whitney Lubin <justwhitneylubin@gmail.com>
2019-08-10 08:17:08 -0400

    seeddb

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,44 +12,69 @@ Location.destroy_all
 HousingType.destroy_all
 User.destroy_all
 
-locations = [
+zipcodes = [
     {
-        "name" => "Space Called Tribe",
-        "phone" => "321-123-1234",
-        "address" => "456 elmo street Miami, FL",
-        "description" => "2 bedroom townhouses",
-        "units" => 15,
-        "zipcode" => "33113",
-        "housing_type" => "rent",
-        "user" => "student"
+        "name" => "33113",
+        "location" => "A"
     },
     {
-        "name" => "Groovin Bean",
-        "phone" => "123-321-1234",
-        "address" => "123 elmo street Miami, FL",
-        "description" => "1 bedroom condos",
-        "units" => 22,
-        "zipcode" => "33132",
-        "housing_type" => "rent",
-        "user" => "parent"
-        
+        "name" => "33132",
+        "location" => "B"
     },
     {
-        "name" => "Whitney Condo",
-        "phone" => "954-744-0000",
-        "address" => "1261 sw 103 ave Miami, FL",
-        "description" => "3 bedroom single family ranch style house",
-        "units" => 1,
-        "zipcode" => "33313",
-        "housing_type" => "buy",
-        "user" => "veteran"
+        "name" => "33313",
+        "location" => "C"
+    }
+
 ]
 
-locations.each do |x|
-    location = Location.create(name: x["name"], x["phone"], x["address"], x["drescription"], x["units"], x["zipcode"])
-    Zipcode.create(name: x["zipcode"], location_id: location:id)
-    HousingType.create(name: x["housing_type"], location_id: location:id)
-    User.create(name: x["user"], location_id:)
+
+# locations = [
+#     {
+#         "name" => "Space Called Tribe",
+#         "phone" => "321-123-1234",
+#         "address" => "456 elmo street Miami, FL",
+#         "description" => "2 bedroom townhouses",
+#         "units" => 15,
+#         "zipcode" => "33113",
+#         "housing_type" => "rent",
+#         "user" => "student"
+#     },
+#     {
+#         "name" => "Groovin Bean",
+#         "phone" => "123-321-1234",
+#         "address" => "123 elmo street Miami, FL",
+#         "description" => "1 bedroom condos",
+#         "units" => 22,
+#         "zipcode" => "33132",
+#         "housing_type" => "rent",
+#         "user" => "parent"
+        
+#     },
+#     {
+#         "name" => "Whitney Condo",
+#         "phone" => "954-744-0000",
+#         "address" => "1261 sw 103 ave Miami, FL",
+#         "description" => "3 bedroom single family ranch style house",
+#         "units" => 1,
+#         "zipcode" => "33313",
+#         "housing_type" => "buy",
+#         "user" => "veteran"
+#     }
+# ]
+
+# locations.each do |x|
+#     location = Location.create(name: x["name"], phone: x["phone"], address: x["address"], description: x["drescription"], units: x["units"], zipcode: x["zipcode"])
+#     zipcode = Zipcode.create(name: x["zipcode"], location_id: location:id)
+#     housing_type = HousingType.create(name: x["housing_type"], location_id: location:id)
+#     user = User.create(name: x["user"], location_id:)
+# end
+
+zipcodes.each do |x|
+    zipcode = Zipcode.create(name: x["name"])
+    location = Location.create(name: x["name"], phone: x["phone"], address: x["address"], description: x["drescription"], units: x["units"], zipcode_id: x["zipcode:id"], user_id: x["user:id"], housing_type_id: x["housing_type:id"])
+    # housing_type = HousingType.create(name: x["housing_type"])
+    # user = User.create(name: x["user"])
 end
 
 put "There are #{Location.count} afforable housing Location options available"
