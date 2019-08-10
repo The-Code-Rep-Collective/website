f966e904fc25d6ef44f13617b5809e52020d552a
Whitney Lubin <justwhitneylubin@gmail.com>
2019-08-10 10:26:49 -0400

    new endpoints defined and working

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -1,9 +1,9 @@
 module Api
-    module v1
+    module V1
         class LocationsController < ApplicationController
             def index
                 locations = Location.order('created_at DESC');
-                render json: {status: 'SUCCESS', message: 'Loaded Locations', data:locations}, status:ok
+                render json: {status: 'SUCCESS', message: 'Loaded Locations', data:locations}, status: :ok
             end
         end
     end
