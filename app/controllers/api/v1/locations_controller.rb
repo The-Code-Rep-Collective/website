83ea949c7d6d8087ba691102eea7b13c347deb20
Whitney Lubin <justwhitneylubin@gmail.com>
2019-08-10 11:55:15 -0400

    endpoint for single location listing bu id number

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -5,6 +5,10 @@ module Api
                 locations = Location.order('created_at DESC');
                 render json: {status: 'SUCCESS', message: 'Loaded Locations', data:locations}, status: :ok
             end
+            def show
+                location = Location.find(params[:id]);
+                render json: {status: 'SUCCESS', message: 'Loaded Location', data:location}, status: :ok
+            end
         end
     end
 end
